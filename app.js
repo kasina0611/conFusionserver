7087863a14027b56a1a129a6fcf7bb7005e04084
kasina0611 <saiganesh01.k@gmail.com>
2021-06-21 18:20:14 +0530

    file upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter  = require('./routes/dishRouter');
 var promoRouter  = require('./routes/promoRouter');
 var leaderRouter  = require('./routes/leaderRouter');
+var uploadRouter  = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const url=config.mongoUrl;
@@ -23,6 +24,14 @@ connect.then((db)=>{
   console.log('Connected correctly to Server');
 },(err)=>{console.log(err);});
 var app = express();
+
+app.all('*',(req,res,next)=>{
+  if(req.secure){
+    return next();
+  }else{
+    res.redirect(307,'https://'+req.hostname+':'+app.get('secPort')+req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
