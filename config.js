78037b9280bd04dd27629e779547bd9b8796d3fc
kasina0611 <saiganesh01.k@gmail.com>
2021-06-22 22:55:10 +0530

    oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports={
     'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'126804212828959',
+        clientSecret:'677bcdc027092e3825ec0c92c2888dda'
+    }
 }
